20e75dd60bcd60d23bc318022b0d6e9d1b2b2de5
Ursyne <talisker@tuta.io>
2020-08-04 12:41:43 -0400

    WIP

--- Godot/Milestone-01/MusicPlayer.gd
+++ Godot/Milestone-01/MusicPlayer.gd
@@ -8,12 +8,16 @@ onready var tracks = [
   'music_three',
   'music_four',
   'music_five',
-  'music_six'
+  'music_six',
+  'music_seven',
+  'music_eight',
+  'music_nine',
+  'music_ten'
 ]
 
 func _ready():
 	random.randomize()
-	var rand_nb = random.randi_range(0,5)
+	var rand_nb = random.randi_range(0,9)
 	var audiostream = load('res://Assets/Music/' + tracks[rand_nb] + '.ogg')
 	set_stream(audiostream)
 	play()
